6af872bc4f920c318df12fb6d87ff5a14e5d1c63
EDvanis1 <edgaramani99@gmail.com>
2024-08-14 10:05:43 +0000

    calcule.c

---
+++ calcule.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int tableDeMultiplication(int nombre){
+   for ( i = 0; i < 10; i++){
+      printf(" %d * %d = ", nombre, i);
+      return nombre * i;
+   }
+   
+}
