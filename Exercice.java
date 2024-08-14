ea4c72170be2cf3741ecf8f47e5ba73f73111b7e
EDvanis1 <edgaramani99@gmail.com>
2024-08-14 10:02:14 +0000

    Exercice.java

---
+++ Exercice.java
@@ -0,0 +1,18 @@
+package lmms;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class Ecercice {
+    abstract class Main{
+        public String fname = "John";
+        public String lname = "Doe";
+        public String email = "John@doe.com";
+        public int age = 24;
+        public static void study(){
+ }
+    }
+ }
