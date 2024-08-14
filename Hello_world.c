586a4fc05977c919bdfd7c8cd113db389da42465
EDvanis1 <edgaramani99@gmail.com>
2024-08-14 10:04:02 +0000

    Hello_world.c

---
+++ Hello_world.c
@@ -0,0 +1,18 @@
+/*
+Ci-dessous, ce sont des directives de préprocesseur.
+Ces lignes permettent d'ajouter des dichiers au projet, fichiers que l'on appelle bibliothèques.
+Grâce à ces bibliothèques, on disposera de fonctions toutes prêtes pour afficher par exemple un message à l'écran.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Ci-dessous, vous avez la fonction principale du programme, appellée main.
+C'est par cette fonction que tous les programmes commencent.
+Ici, ma fonction se contente d'afficher Bonjour à l'écran.
+*/
+
+int main(){
+    printf("Moi c'est EDvanis\n");
+    return 0;
+}
